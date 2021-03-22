106f63d702305b272cb8d0ee9a3b4c51a3e5256e
Prateek Antony <prateekantony@gmail.com>
2021-03-22 11:56:30 +0000

    3rd commit

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('testing')
+// get quote from API
+async function getQuote()
+{
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try
+    {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await rsponse.json();
+    }
+    catch (error)
+    {
+        console.log('Error')
+    }
+}
+// On load
+getQuote();
