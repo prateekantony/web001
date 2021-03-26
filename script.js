b099ed5f6d8064ed865dce45dfc8a5ea3d575817
Prateek Antony <prateekantony@gmail.com>
2021-03-26 15:26:55 +0000

    API quotes added, minor design changes

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+let data = [];
+let rnum;
 // get quote from API
+function singleQuote(qarray)
+{
+    rnum = Math.floor(Math.random() * qarray.length);
+    return qarray[rnum];s
+}
+
 async function getQuote()
 {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const apiUrl =
+    "https://type.fit/api/quotes/?method=getQuote&lang=en&format=json";
 
     try
     {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await rsponse.json();
+        const response = await fetch(apiUrl);
+        data = await response.json();
+        let newQuote = singleQuote(data);
+        console.log(newQuote);
+        if (newQuote.author === null)
+        {
+            authorText.textContent = "Unknown";
+        }
+        else
+        {
+            authorText.textContent = newQuote.author;
+        }
+        quoteText.textContent = newQuote.text
+        
     }
     catch (error)
     {
-        console.log('Error')
+        console.log(error);
     }
 }
 // On load
